de58a5fee250d5956ea31df23b28cdab8015eb1c
Janelle K Hirano <jaahnelle@gmail.com>
2019-06-05 22:23:43 -1000

    added myName variable, added person variable

--- basics.js
+++ basics.js
@@ -1,9 +1,17 @@
 /* Create a `myName` variable and assign it a String value */
 
+var myName = 'Janelle';
+
 /* Create a `person` variable and give it 2 properties,
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+
+ var person = {
+     name: 'Janelle',
+     age: 30
+ }
+
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
